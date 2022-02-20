8098ad5eee15b2681d0dc1e10a0f866dd2e85766
Nicolas THIMONIER <nthimoni@e2r9p4.clusters.42paris.fr>
2022-02-20 20:35:37 +0100

    gogo

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 18:40:10 by nthimoni          #+#    #+#             */
-/*   Updated: 2022/02/19 18:03:42 by nthimoni         ###   ########.fr       */
+/*   Updated: 2022/02/20 19:08:13 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,28 @@
 #include "philo.h" 
 
 
-int init_mutex(t_info *info, t_philo *philo)
+int init_mutex(t_info *info, t_philo **philo)
 {
 	int	i;
 
 	pthread_mutex_init(&info, NULL);
-	philo = malloc(sizeof(t_philo) * info->nb_philo);
+	*philo = malloc(sizeof(t_philo) * info->nb_philo);
+	if (!philo)
+		return (-1);
+	info->fork = malloc(sizeof(pthread_mutex_t) * info->nb_philo);
+	if (!fork)
+		return (-2);
 	i = 0;
-
 	while (i < info->nb_philo)
 	{
+
 		philo[i].id = i + 1;
 		philo[i].last_eat = 0;
+		philo[i].l_fork = &info->fork[i];
+		if (i != 0)
+			philo[i].r_fork = &info->fork[i - 1];
+		else
+			philo[i].r_fork = &info->fork[info->nb_philo - 1];
 		i++;
 	}
 }
@@ -36,19 +46,32 @@ int	destroy_mutex(t_info *info, t_philo *philo)
 	pthread_mutex_destroy(&info);
 }
 
+int	is_dead(t_info *info, t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < info->nb_philo)
+	{
+		if (get_time(info) - philo[i].last_eat > info->time_to_die)
+		{
+			info->is_finish = 1;
+			log(philo[i].id, DIE);
+			return (1);
+		}
+	}
+	return (0);
+}
+
 int main(int argc, char *argv[])
 {
-	int old;
-	int tmp;
-	t_info info;
+	int		old;
+	int		tmp;
+	t_info	info;
 
 	old = 0;
 	if (error_parse(parse(argc, argv, &info)))
 		return (-1);
-	while (info->is_finish == 0)
-	{
-		pthread_mutex_unlock(&info->mut_fin);
+	while (!is_dead(&info, philo))
 		usleep(2);
-		pthread_mutex_lock(&info->mut_fin);
-	}
 }
--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 16:44:52 by nthimoni          #+#    #+#             */
-/*   Updated: 2022/02/19 18:16:12 by nthimoni         ###   ########.fr       */
+/*   Updated: 2022/02/20 18:32:56 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ void	routine(void *arg)
 	t_philo	*philo;
 
 	philo = arg;
-	while (get_time(philo->info) - philo->last_eat < philo->info->time_to_die)
+	while (!philo->info->is_finish)
 	{
+		pthread_mutex_unlock(&info->mut_fin);
+		
+		pthread_mutex_lock(&info->mut_fin);
 	}
-	log(philo->id, DIE);
 }
